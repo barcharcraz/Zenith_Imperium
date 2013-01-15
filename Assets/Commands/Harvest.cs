21743e4425f32cef94698d1eaeaaaf9674ce10fa
Charlie Barto <bartoc@umich.edu>
2013-01-15 16:37:28 -0500

    resourcs things

---
+++ Assets/Commands/Harvest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commands
+{
+    class Harvest : ITargetedCommand<ResourceController>
+    {
+
+        public void exec(UnitController controller, ResourceController target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { return "Harvest"; }
+        }
+
+        public void exec(BasicController controller)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Commands/TargetedCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Events;
+namespace Commands
+{
+    public abstract class TargetedCommand<T> : ITargetedCommand<T>
+    {
+
+
+        private BasicController m_controller;
+        protected abstract T getTarget(ClickEventArgs e);
+        public void exec(BasicController controller)
+        {
+            controller.Owner.SendCommand += Owner_SendCommand;
+        }
+
+        void Owner_SendCommand(object sender, ClickEventArgs e)
+        {
+            exec()
+        }
+    }
+}
