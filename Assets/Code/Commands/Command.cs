d8eac833b959c2a0f72cc6386557dd2a4378fce5
Charlie Barto <barcharcraz@msn.com>
2013-03-29 12:15:28 -0400

    more command stuff

--- Assets/Code/Commands/Command.cs
+++ Assets/Code/Commands/Command.cs
@@ -6,27 +6,25 @@ using UnityEngine;
 
 namespace Commands
 {
-    public delegate void CommandFinishedHandler(Command src, params System.Object[] retval);
-    public delegate void CommandAddHandler(params Type[] commands);
+	public delegate void CommandFinishedHandler(Command src, params System.Object[] retval);
+	public delegate void CommandAddHandler(params Type[] commands);
 	public abstract class Command
 	{
-        public event CommandAddHandler AddCommands;
-        public event CommandFinishedHandler Finished;
-        //specify the type signature of the returned values
-        public virtual Type[] ReturnType { get { return null; } }
+		public event CommandAddHandler AddCommands;
+		public event CommandFinishedHandler Finished;
+		//specify the type signature of the returned values
+		public virtual Type[] ReturnType { get { return null; } }
 		public CommandManager parent { get; set; }
-        protected virtual void OnFinished(System.Object retval)
-        {
-            Finished(this, retval);
-            
-            
-        }
+		protected virtual void OnFinished(System.Object retval)
+		{
+			Finished(this, retval);
+		}
 		public abstract void Update();
 		
-        protected virtual void OnAddCommands(params Type[] commands)
-        {
-            AddCommands(commands);
-        }
+		protected virtual void OnAddCommands(params Type[] commands)
+		{
+			AddCommands(commands);
+		}
 
 	}
 }
--- Assets/Code/Commands/CommandManager.cs
+++ Assets/Code/Commands/CommandManager.cs
@@ -11,47 +11,47 @@ namespace Commands
 	public class CommandManager : MonoBehaviour
 	{
 		private Command executingCommand;
-        private Queue<Type> m_commandQueue;
+		private Queue<Type> m_commandQueue;
 		public void Start() 
 		{
 			m_commandQueue = new Queue<Type>();
 		}
-        public void handleCommand(Command src, System.Object args)
-        {
-            if (src != null)
-            {
-                src.Finished -= handleCommand;
-                src.AddCommands -= AddCommands;
-            }
-            Type nextCommand = m_commandQueue.Dequeue();
-            if (nextCommand.BaseType != typeof(Commands.Command))
-            {
-                throw new InvalidOperationException("Commands must inherit from Command type " + nextCommand + "does not");
-            }
+		public void handleCommand(Command src, params System.Object[] args)
+		{
+			if (src != null)
+			{
+				src.Finished -= handleCommand;
+				src.AddCommands -= AddCommands;
+			}
+			Type nextCommand = m_commandQueue.Dequeue();
+			if (nextCommand.BaseType != typeof(Commands.Command))
+			{
+				throw new InvalidOperationException("Commands must inherit from Command type " + nextCommand + "does not");
+			}
+            IEnumerable<ConstructorInfo> possibleConst =  GetCompatibleConstructors(nextCommand, src.ReturnType);
+            Command nextCommandinst = possibleConst.First().Invoke(args) as Command;
+			nextCommandinst.Finished += handleCommand;
+			nextCommandinst.AddCommands += AddCommands;
 			
-            executingCommand = nextCommand;
-            addedCommand.Finished += handleCommand;
-            addedCommand.AddCommands += AddCommands;
-            
-        }
-        public void AddCommands(params Type[] commands)
-        {
-            foreach (Type c in commands)
-            {
-                AddCommand(c);
-            }
-        }
-        public void AddCommand(Type command)
-        {
-            m_commandQueue.Enqueue(command);
-            //only want to kick off if the previously added command is the only
-            //command in the queue
-            if (m_commandQueue.Count == 1)
-            {
-                handleCommand(null, null);
-            }
-            
-        }
+		}
+		public void AddCommands(params Type[] commands)
+		{
+			foreach (Type c in commands)
+			{
+				AddCommand(c);
+			}
+		}
+		public void AddCommand(Type command)
+		{
+			m_commandQueue.Enqueue(command);
+			//only want to kick off if the previously added command is the only
+			//command in the queue
+			if (m_commandQueue.Count == 1)
+			{
+				handleCommand(null, null);
+			}
+			
+		}
 		public void Update() 
 		{
 			if(executingCommand != null) 
@@ -61,13 +61,12 @@ namespace Commands
 		}
 		private IEnumerable<ConstructorInfo> GetCompatibleConstructors(Type command, params Type[] signature)
 		{
-            IEnumerable<ConstructorInfo> retval = from ConstructorInfo c in command.GetConstructors()
-                                                  where c.GetParameters().IsEqual(signature)
-                                                  select c;
-            return retval;
+			IEnumerable<ConstructorInfo> retval = from ConstructorInfo c in command.GetConstructors()
+												  where c.GetParameters().IsEqual(signature)
+												  select c;
+			return retval;
 		}
-        private Type[] InterperetReturnValue(params System.Object[] returnVal)
-        
-        
+		
+		
 	}
 }
