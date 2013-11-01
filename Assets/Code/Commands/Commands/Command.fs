90f1fa2d8c65ab77b349c6f4b972daaf0b4a12f4
Charles Augustin Barto <bartoc@caen-vnc02.engin.umich.edu>
2013-10-31 20:45:35 -0400

    added fs command library

---
+++ Assets/Code/Commands/Commands/Command.fs
@@ -0,0 +1,32 @@
+﻿namespace Commands
+open UnityEngine
+
+
+type CommandTarget = Position of Vector3 |  Target of GameObject
+type Command = {
+    target : CommandTarget
+
+}
+type AttackCommand = {
+    target : Choice<Vector3, GameObject>
+    source : GameObject
+}
+type MoveCommand = {
+    target : Vector3
+    source : GameObject
+}
+type HarvestCommand = {
+    target : GameObject
+    source : GameObject
+    drop_location : GameObject
+}
+
+type CommandType = Attack of AttackCommand
+                  |Move of MoveCommand
+                  |Harvest of HarvestCommand
+
+
+
+ 
+type Class1() = 
+    member this.X = "F#"
